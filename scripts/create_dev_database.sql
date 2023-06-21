274f63feb6f1377cb9c621587f1abc182fa364ab
Vasily Korovin <vskorovin@gmail.com>
2023-06-21 19:02:33 +0300

    New project HomeWork11

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER hw11 SUPERUSER;
+CREATE DATABASE hw11 OWNER hw11 ENCODING 'utf-8';
